034ecddce78b992c43a8b337bb37c4ca836bcbbe
Albercook <albercook@gmail.com>
2015-04-23 12:28:02 -0400

    Describe each concept first time it used
    Better names, faster blink, many more comments.

--- Programs/Arduino/Heli/Heli_Prog/_1Blink_2014/_1Blink_2014.ino
+++ Programs/Arduino/Heli/Heli_Prog/_1Blink_2014/_1Blink_2014.ino
@@ -1,12 +1,29 @@
-int led = 3;     //Declares Var led to pin 3
+// There is a red LED on connected to a pin on the processor.
+// Referring to it by number is not very informative, 
+// particularly, if a future version has the red LED connected to
+// another pin.
+// We will give it a name, and only refer to it by that name everywhere.
+// Only the next line needs to know which pin has the red LED.
+int redLED = 3; // The red LED is on pin 3 in this hardware version. 
+
+// setup() gets called once before anything else. 
+// You will see it in each of these programs.
+// Remember this, as we will just assume you know this
+// in all of the other examples.
 void setup()
 {
-  pinMode(led, OUTPUT);    //Declares pin 3 to be an output   
+  pinMode(redLED, OUTPUT);    //Declares the pin with the red LED to be an output   
 }
-for(x, x < 10, x++)   //repeats while x is < 10 x goes up by a constant 1
+
+// loop() gets called repeatedly.
+void loop()
 {
-  digitalWrite(led, HIGH); //sets led on HIGH
-  delay(1000);    //waits 1 sec
-  digitalWrite(led, LOW);    //sets led to LOW
-  delay(1000);   //waits 1 sec
+  digitalWrite(redLED, HIGH); // The LED is connected through a resistor
+                              // to ground. Setting the pin high, turns
+                              // the LED on.
+  delay(100);    //wait one tenth of second, so the human can see the LED.
+  digitalWrite(redLED, LOW);    // Setting the pin low, means this side
+                                // of the LED is also electrically connected
+                                // to ground. So the LED is off.
+  delay(900);   //Leave it off for 9 tenths of a second.
 }
